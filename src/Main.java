806f61d0f5ee678ecd56e779173037a677b9b1f1
Aleksandar Miloeski <a.miloeski@wickitly.com>
2022-11-21 09:27:11 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Input: ");
+        
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        
+        Animal a = new Animal("cat");
+    }
+    
+//    private static String readInput() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Input: ");
+//        return scanner.nextLine();
+//    }
+    
+//    static void bla()
+//    {
+//        String str = "12 + 14";
+//        String[] parts = str.split(" ");
+//        System.out.println(Arrays.toString(parts));
+//    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
